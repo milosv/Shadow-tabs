36707135169ca40e55b090dcb70b00236aab7190
milosv <milos@milosv.com>
2011-09-29 17:28:34 -0300

    comments

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-/* Author: 
+/* Author: Milos
 
 */
+//this code is a bit of a mess but I just wanted to see how tabs work as a JS tabs.
 $('a').click(function(){
+	//check if we are dealing with first element or not
 	if ($(this).html() == $('ul li:first a').html())
 	{
 		$('.cur_first').removeClass('cur_first');
